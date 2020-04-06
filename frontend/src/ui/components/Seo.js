7c476c767ff6c6f0c7fa69eb12afd0fca135144f
Leonardo Galante <lndgalante@gmail.com>
2020-04-06 05:19:34 -0300

    Remove logs

--- frontend/src/ui/components/Seo.js
+++ frontend/src/ui/components/Seo.js
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
 export const SEO = ({ description, lang, meta, title }) => {
-  console.log('SEO -> meta', meta)
   const { site } = useStaticQuery(
     graphql`
       query {
