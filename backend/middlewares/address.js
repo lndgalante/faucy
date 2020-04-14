2894f44e4356bd8a6e61b068c21d74ba515713eb
Leonardo Galante <lndgalante@gmail.com>
2020-04-13 22:14:15 -0300

    Remove unnecesary comments

--- backend/middlewares/address.js
+++ backend/middlewares/address.js
@@ -3,12 +3,10 @@ const { isAddress } = require('ethereum-address');
 function userAddress(req, res, next) {
   const { address } = req.body;
 
-  // Check address
   if (!address) {
     return res.status(400).send({ message: 'Address not defined' });
   }
 
-  // Validate address
   if (!isAddress(address)) {
     return res.status(422).send({ message: 'Invalid address' });
   }
