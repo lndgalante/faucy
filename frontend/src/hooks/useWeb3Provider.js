5f773880f31b73396aba5521963041cb146f406f
Leonardo Galante <lndgalante@gmail.com>
2020-04-18 04:01:57 -0300

    🐛 FIX: SSR build by checking window

--- frontend/src/hooks/useWeb3Provider.js
+++ frontend/src/hooks/useWeb3Provider.js
@@ -1,9 +1,11 @@
 import { useMemo } from 'react'
 import { ethers } from 'ethers'
 
+const isDOMavailable = !!(typeof window !== 'undefined' && window.document && window.document.createElement)
+
 const useWeb3Provider = () => {
   const web3Provider = useMemo(
-    () => (window?.web3 ? new ethers.providers.Web3Provider(window?.web3?.currentProvider) : null),
+    () => (isDOMavailable && window?.web3 ? new ethers.providers.Web3Provider(window?.web3?.currentProvider) : null),
     [],
   )
 
