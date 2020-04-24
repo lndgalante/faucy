a2dfc2bd23408f2660b6fb376659b61a6adc97f4
Leonardo Galante <lndgalante@gmail.com>
2020-04-24 16:18:57 -0300

    👌 IMPROVE: 404 page layout

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Box, Text } from '@chakra-ui/core';
 
 // Components
 import { SEO } from '../ui/components/Seo';
 
 const NotFoundPage = () => (
-  <div>
+  <Box alignItems="center" d="flex" height="100vh" justifyContent="center" p={4} w="100%">
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
+  </Box>
 );
 
 export default NotFoundPage;
