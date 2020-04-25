c974aa8167c314a63c7a57f8f348472b66dbe66d
Leonardo Galante <lndgalante@gmail.com>
2020-04-25 00:24:16 -0300

    👌 IMPROVE: CORS setup

--- backend/utils/express.js
+++ backend/utils/express.js
@@ -10,10 +10,22 @@ const rateLimit = require('express-rate-limit');
 // Custom Middlewares
 const { userAddress } = require('../middlewares/address');
 
+function getCorsOptions() {
+  const whitelist = ['http://localhost:8000', 'https://faucy.eth'];
+
+  const corsOptions = {
+    origin(origin, callback) {
+      return whitelist.includes(origin) ? callback(null, true) : callback(new Error('Not allowed by CORS'));
+    },
+  };
+
+  return corsOptions;
+}
+
 function setupExpressApp() {
   const app = express();
 
-  app.use(cors());
+  app.use(cors(getCorsOptions()));
   app.use(helmet());
 
   app.use(bodyParser.json());
