c81ce244198dd870347c666aea9a57a8504c5b08
Leonardo Galante <lndgalante@gmail.com>
2020-04-27 21:21:22 -0300

    👌 IMPROVE: Add query-string to support proxy setup

--- backend/utils/puppeteer.js
+++ backend/utils/puppeteer.js
@@ -1,3 +1,4 @@
+const queryString = require('query-string');
 const puppeteer = require('puppeteer-extra');
 const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
 
@@ -9,8 +10,10 @@ const IS_PRODUCTION = NODE_ENV === 'production';
 puppeteer.use(RecaptchaPlugin({ provider: { id: '2captcha', token: TWO_CAPTCHA_API_TOKEN }, visualFeedback: true }));
 
 function getBrowser() {
-  const browserWSEndpoint = `wss://chrome.browserless.io?token=${BROWSERLESS_API_TOKEN}`;
-  return IS_PRODUCTION ? puppeteer.connect({ browserWSEndpoint }) : puppeteer.launch({ headless: false });
+  const browserlessQuery = queryString.stringify({ token: BROWSERLESS_API_TOKEN, '--proxy-server': undefined });
+  const browserWSEndpoint = `wss://chrome.browserless.io?${browserlessQuery}`;
+
+  return !IS_PRODUCTION ? puppeteer.connect({ browserWSEndpoint }) : puppeteer.launch({ headless: false });
 }
 
 module.exports = { getBrowser };
