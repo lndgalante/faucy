323b9fb806abf12396cf68aadb3c906b4ff9f853
Leonardo Galante <lndgalante@gmail.com>
2020-04-29 22:35:21 -0300

    👌 IMPROVE: Add proxy to puppeteer

--- backend/utils/puppeteer.js
+++ backend/utils/puppeteer.js
@@ -3,17 +3,19 @@ const puppeteer = require('puppeteer-extra');
 const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
 
 // Constants - Environment variables
-const { BROWSERLESS_API_TOKEN, TWO_CAPTCHA_API_TOKEN, NODE_ENV } = process.env;
+const { BROWSERLESS_API_TOKEN, TWO_CAPTCHA_API_TOKEN, PROXY_SERVER_URL, NODE_ENV } = process.env;
 const IS_PRODUCTION = NODE_ENV === 'production';
 
 // Puppeteer - Plugins
 puppeteer.use(RecaptchaPlugin({ provider: { id: '2captcha', token: TWO_CAPTCHA_API_TOKEN }, visualFeedback: true }));
 
 function getBrowser() {
-  const browserlessQuery = queryString.stringify({ token: BROWSERLESS_API_TOKEN, '--proxy-server': undefined });
+  const browserlessQuery = queryString.stringify({ token: BROWSERLESS_API_TOKEN, '--proxy-server': PROXY_SERVER_URL });
   const browserWSEndpoint = `wss://chrome.browserless.io?${browserlessQuery}`;
 
-  return !IS_PRODUCTION ? puppeteer.connect({ browserWSEndpoint }) : puppeteer.launch({ headless: false });
+  return !IS_PRODUCTION
+    ? puppeteer.connect({ browserWSEndpoint })
+    : puppeteer.launch({ headless: false, args: [`--proxy-server=${PROXY_SERVER_URL}`] });
 }
 
 module.exports = { getBrowser };
