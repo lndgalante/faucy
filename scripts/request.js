140fe5bad9d760cdf666af6c82154232dba4592c
Leonardo Galante <lndgalante@gmail.com>
2020-08-12 03:29:37 -0300

    👌 IMPROVE: Script logic

--- scripts/request.js
+++ scripts/request.js
@@ -2,6 +2,8 @@ const delay = require('delay');
 const dotenv = require('dotenv');
 const wretch = require('wretch');
 const fetch = require('node-fetch');
+const Web3 = require('web3');
+const EthereumTx = require('ethereumjs-tx');
 
 // Initial config
 dotenv.config();
@@ -24,23 +26,82 @@ const services = {
 
 const getNetworkService = (networkName) => services[networkName];
 
-async function requestEthers(address, network) {
-  console.log('requestEthers -> address', address);
-  console.log('requestEthers -> network', network);
+const getCurrentGasPrices = async () => {
+  const { safeLow, average, fast } = await wretch().url('https://ethgasstation.info/json/ethgasAPI.json').get().json();
 
+  const prices = {
+    low: safeLow / 10,
+    medium: average / 10,
+    high: fast / 10,
+  };
+
+  console.log('\r\n');
+  console.log(`Current ETH Gas Prices (in GWEI):`);
+  console.log('\r\n');
+  console.log(`Low: ${prices.low} (transaction completes in < 30 minutes)`);
+  console.log(`Standard: ${prices.medium} (transaction completes in < 5 minutes)`);
+  console.log(`Fast: ${prices.high} (transaction completes in < 2 minutes)`);
+  console.log('\r\n');
+
+  return prices;
+};
+
+const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/0dd208d9f5cd4d5d9a10f196eb15cb61'));
+
+async function requestEthers(network) {
   try {
+    // sender wallet
+    const sendWallet = '0x1DbB9983148705C84eFf23d93F43b12920DEF6c3';
+
+    // create account
+    const { address, privateKey } = web3.eth.accounts.create();
+    console.log('requestEthers -> address', address);
+
+    // get nonce from my wallet
+    const nonce = await web3.eth.getTransactionCount(address);
+    console.log('requestEthers -> nonce', nonce);
+
+    // get gas prices
+    const gasPrices = await getCurrentGasPrices();
+    console.log('requestEthers -> gasPrices', gasPrices);
+
+    // get ethers
     const networkService = getNetworkService(network);
-    const { body } = await networkService(address);
-    console.log('requestEthers -> body', body);
+    const response = await networkService(address);
+    console.log('requestEthers -> response', response);
+
+    // tx details
+    const amountToSend = 0.2;
+    const details = {
+      nonce,
+      gas: 21000,
+      chainId: 4,
+      to: sendWallet,
+      gasPrice: gasPrices.low * 1000000000,
+      value: web3.utils.toHex(web3.utils.toWei(amountToSend, 'ether')),
+    };
+
+    // create tx
+    const transaction = new EthereumTx(details);
+
+    // sign tx
+    transaction.sign(Buffer.from(privateKey, 'hex'));
+    const serializedTransaction = transaction.serialize();
+
+    // send tx
+    const transactionId = web3.eth.sendRawTransaction('0x' + serializedTransaction.toString('hex'));
+    const url = `https://rinkeby.etherscan.io/tx/${transactionId}`;
+    console.log('requestEthers -> url', url);
+
     await delay(5000);
 
-    requestEthers(address, network);
+    requestEthers(network);
   } catch (error) {
     console.log('requestEthers -> error', error);
   }
 }
 
-requestEthers('0x1DbB9983148705C84eFf23d93F43b12920DEF6c3', 'rinkeby');
+requestEthers('rinkeby');
 
 /* Idea:
   Create account (Web3) -> Send to that new account ->  Send the eth from that account to another one
